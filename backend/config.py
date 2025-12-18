8cfaf5ad3f03b0877c2e7846996882353bacf098
Vincent Manalo <146041946+vnmw7@users.noreply.github.com>
2025-12-19 00:16:31 +0800

    feat: add additional CORS origins for enhanced API accessibility

--- backend/config.py
+++ backend/config.py
@@ -46,6 +46,8 @@ class Settings(BaseSettings):
     BACKEND_CORS_ORIGINS: list[str] = [
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://itl411-pokemon-api.vercel.app",
+        "https://www.pokedex.systems"
     ]
 
     # ML Model parameters
